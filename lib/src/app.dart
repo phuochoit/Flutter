3f4686ddf10d6afb2e2190da4b2847d6b8fd975d
Phuochoit <phuochoit@gmail.com>
2019-03-18 15:55:56 +0700

    Demo APP

---
+++ lib/src/app.dart
@@ -0,0 +1,11 @@
+import 'package:demoapp/src/resources/login_page.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: LoginPage(),
+    );
+  }
+}
---
+++ lib/src/resources/login_page.dart
@@ -0,0 +1,162 @@
+import 'package:demoapp/src/blocs/login_bloc.dart';
+import 'package:demoapp/src/resources/home_page.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  LoginBloc bloc = new LoginBloc();
+  bool _showPass = false;
+  var _userNameError = "User Name error!";
+  var _passError = "PassWord Error!";
+  var _userNameInvalid = false;
+  var _passInvalid = false;
+  TextEditingController _userController = new TextEditingController();
+  TextEditingController _passController = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+        constraints: BoxConstraints.expand(),
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: Container(
+                  width: 70,
+                  height: 70,
+                  padding: EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: Color(0xffd8d8d8d)),
+                  child: FlutterLogo()),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 30),
+              child: Text(
+                "Hello \n Welcome Back",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    fontSize: 30),
+              ),
+            ),
+            Padding(
+                padding: const EdgeInsets.only(bottom: 20),
+                child: StreamBuilder(
+                  stream: bloc.userNameStream,
+                  builder: (context, snapshot) => TextField(
+                        style: TextStyle(fontSize: 18, color: Colors.black),
+                        controller: _userController,
+                        decoration: InputDecoration(
+                            labelText: "User Name",
+                            errorText: _userNameInvalid ? _userNameError : null,
+                            labelStyle: TextStyle(
+                                color: Color(0xff888888), fontSize: 15)),
+                      ),
+                )),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 30),
+              child: Stack(
+                alignment: AlignmentDirectional.centerEnd,
+                children: <Widget>[
+                  StreamBuilder(
+                      stream: bloc.passWordStream,
+                      builder: (context, snapshot) => TextField(
+                            style: TextStyle(fontSize: 18, color: Colors.black),
+                            obscureText: !_showPass,
+                            controller: _passController,
+                            decoration: InputDecoration(
+                                labelText: "PassWord",
+                                errorText: _passInvalid ? _passError : null,
+                                labelStyle: TextStyle(
+                                    color: Color(0xff888888), fontSize: 15)),
+                          )),
+                  GestureDetector(
+                    onTap: onToogleShowPass,
+                    child: Text(_showPass ? "HIDE" : "SHOW",
+                        style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 13,
+                            fontWeight: FontWeight.bold)),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 0),
+              child: SizedBox(
+                width: double.infinity,
+                height: 56,
+                child: RaisedButton(
+                  color: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8))),
+                  onPressed: onSignInClicked,
+                  child: Text(
+                    "SIGN IN",
+                    style: TextStyle(color: Colors.white, fontSize: 14),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 100,
+              width: double.infinity,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "NEW USER? SIGN UP",
+                    style: TextStyle(fontSize: 15, color: Color(0xff888888)),
+                  ),
+                  Text(
+                    "FORGOT PASSWORD",
+                    style: TextStyle(fontSize: 15, color: Colors.blue),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void onToogleShowPass() {
+    setState(() {
+      _showPass = !_showPass;
+    });
+  }
+
+  void onSignInClicked() {
+    setState(() {
+      if (_userController.text.length < 6 ||
+          !_userController.text.contains("@")) {
+        _userNameInvalid = true;
+      } else {
+        _userNameInvalid = false;
+      }
+
+      if (_passController.text.length < 6) {
+        _passInvalid = true;
+      } else {
+        _passInvalid = false;
+      }
+
+      if (!_userNameInvalid && !_passInvalid) {
+        Navigator.push(context, MaterialPageRoute(builder: gotoHome));
+      }
+    });
+  }
+
+  Widget gotoHome(BuildContext context) {
+    return HomePage();
+  }
+}
