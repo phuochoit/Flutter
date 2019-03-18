da3392904cb8c3125ac92457305e0a1f13a28084
Phuochoit <phuocck8@gmail.com>
2019-03-18 22:22:53 +0700

    update fordel demoapp to login

--- Login/lib/main.dart
+++ Login/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:demoapp/src/app.dart';
+import 'package:Login/src/app.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
--- Login/lib/src/app.dart
+++ Login/lib/src/app.dart
@@ -1,4 +1,4 @@
-import 'package:demoapp/src/resources/login_page.dart';
+import 'package:Login/src/resources/login_page.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
