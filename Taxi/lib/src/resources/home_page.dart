74e6f526fa3b0e6b0052a9f590ce029c6bb809ea
Phuochoit <phuochoit@gmail.com>
2019-04-01 15:22:02 +0700

    update code

--- Taxi/lib/src/resources/home_page.dart
+++ Taxi/lib/src/resources/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:taxi/src/resources/widget/home_menu.dart';
+import 'package:taxi/src/resources/widget/ride_picker.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,13 +9,61 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  var _scaffoldKey = new GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      constraints: BoxConstraints.expand(),
-      color: Colors.white,
-      child: Center(
-        child: Text("HOME PAGE"),
+    return Scaffold(
+      key: _scaffoldKey,
+      body: SafeArea(
+        child: Container(
+          constraints: BoxConstraints.expand(),
+          color: Colors.white,
+          child: Stack(
+            children: <Widget>[
+              GoogleMap(
+                initialCameraPosition: CameraPosition(
+                    target: LatLng(37.42796133580664, -122.085749655962),
+                    zoom: 14.4746),
+              ),
+              Positioned(
+                left: 0,
+                top: 0,
+                right: 0,
+                child: Column(
+                  children: <Widget>[
+                    AppBar(
+                      backgroundColor: Colors.transparent,
+                      elevation: 0.0,
+                      title: Center(
+                        child: Text(
+                          "Taxi App",
+                          style: TextStyle(color: Color(0xff323643)),
+                        ),
+                      ),
+                      leading: FlatButton(
+                        onPressed: () {
+                          // Scaffold.of(context).openDrawer();
+                          _scaffoldKey.currentState.openDrawer();
+                        },
+                        child: Image.asset("ic_menu.png"),
+                      ),
+                      actions: <Widget>[Image.asset("ic_notify.png")],
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20, left: 20, right: 20),
+                      child: RidePicker(),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        child: HomeMenu(),
       ),
     );
   }
---
+++ Taxi/lib/src/resources/widget/ride_picker.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+class RidePicker extends StatefulWidget {
+  @override
+  _RidePickerState createState() => _RidePickerState();
+}
+
+class _RidePickerState extends State<RidePicker> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          boxShadow: [
+            BoxShadow(
+                color: Color(0x88999999), offset: Offset(0, 5), blurRadius: 5.0)
+          ]),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+            top: 32,
+            left: 0,
+            width: 70,
+            height: 40,
+            child: Center(child: Image.asset("ic_line.png")),
+          ),
+          Column(
+            children: <Widget>[
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: FlatButton(
+                  onPressed: () {},
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: double.infinity,
+                    child: Stack(
+                      alignment: AlignmentDirectional.centerStart,
+                      children: <Widget>[
+                        SizedBox(
+                          width: 40,
+                          height: double.infinity,
+                          child: Center(
+                            child: Image.asset("ic_location_black.png",
+                                alignment: AlignmentDirectional.centerStart),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(left: 40, right: 40),
+                          child: Text(
+                            "16/66 Xuân Diệu, thành phố huế",
+                            style: TextStyle(
+                                fontSize: 16, color: Color(0xff323643)),
+                          ),
+                        ),
+                        Positioned(
+                          right: 0,
+                          top: 0,
+                          width: 40,
+                          height: 50,
+                          child: Center(
+                            child: Image.asset("ic_remove_x.png",
+                                alignment: AlignmentDirectional.centerStart),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: FlatButton(
+                  onPressed: () {},
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: double.infinity,
+                    child: Stack(
+                      alignment: AlignmentDirectional.centerStart,
+                      children: <Widget>[
+                        SizedBox(
+                          width: 40,
+                          height: double.infinity,
+                          child: Center(
+                            child: Image.asset("ic_map_nav.png"),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(left: 40, right: 40),
+                          child: Text(
+                            "Home",
+                            style: TextStyle(
+                                fontSize: 16, color: Color(0xff323643)),
+                          ),
+                        ),
+                        Positioned(
+                          right: 0,
+                          top: 0,
+                          width: 40,
+                          height: 50,
+                          child: Center(
+                            child: Image.asset("ic_remove_x.png"),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
